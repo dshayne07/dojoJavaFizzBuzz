0e8336ef07df11e8b98a8b1b64c63279ab546178
dshayne07 <davidshayne2007@gmail.com>
2018-03-05 12:49:48 -0500

    final version

---
+++ FizzBuzz.java
@@ -0,0 +1,19 @@
+public class FizzBuzz{
+    public static String fizzBuzz(int number) {
+        if(number%3 == 0 && number%5 == 0){
+            return "FizzBuzz";
+        }
+        else if(number%3 == 0){
+            return "Fizz";
+        }
+        else if(number%5 == 0){
+            return "Buzz";
+        }
+        else{
+            return String.valueOf(number);
+        }
+    }
+    public static void main(String[] args){
+        System.out.println(fizzBuzz(15));
+    }
+}
